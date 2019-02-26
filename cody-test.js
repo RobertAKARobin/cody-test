a3be7addfa0aa52a326410c21e902919b5b57520
RobertAKARobin <robertgfthomas@gmail.com>
2019-02-26 08:35:01 -0600

    wrap in iife

--- cody-test.js
+++ cody-test.js
@@ -1,70 +1,72 @@
-const style = {
-	highlight: 'background-color:#ffff00; font-weight:bold',
-	test: 'font-weight:bold',
-	pass: '',
-	fail: 'color:#f00; font-weight:bold'
-}
-
-const _test = function(description, suite){
-	console.log(`%c${description}:`, style.highlight)
-
-	const count = {
-		total: 0,
-		pass: 0,
-		fail: 0
+const _test = (function(){
+	const style = {
+		highlight: 'background-color:#ffff00; font-weight:bold',
+		test: 'font-weight:bold',
+		pass: '',
+		fail: 'color:#f00; font-weight:bold'
 	}
-	const _assert = function(aInput, test, bInput){
-		count.total += 1
 
-		const aLog = (aInput instanceof Function ? stripFunctionText(aInput) : aInput)
-		const bLog = (bInput instanceof Function ? stripFunctionText(bInput) : bInput)
-		const testLog = stripFunctionText(test)
-			.replace(/\ba\b/g, aLog)
-			.replace(/\bb\b/g, bLog)
+	function stripFunctionText(inputFunction){
+		return inputFunction
+			.toString()
+			.replace(/^(async )?function.*?\{/, '')
+			.replace(/^.*?=>\s*\{?/, '')
+			.replace(/\}$/, '')
+			.trim()
+	}
 
-		let aValue, bValue, didPass, fatalError
-		try{
-			aValue = (aInput instanceof Function ? aInput() : aInput)
-			bValue = (bInput instanceof Function ? bInput() : bInput)
-			if(test(aValue, bValue)){
-				didPass = true
-			}else{
-				throw new Error('fail')
-			}
-		}catch(e){
-			didPass = false
-			if(e.message !== 'fail'){
-				fatalError = e
-			}
-		}finally{
-			if(didPass){
-				count.pass += 1
-				console.log(`%c${count.total}.) ${testLog}`, style.pass)
-			}else{
-				count.fail += 1
-				console.log(`%c${count.total}.) ${testLog}`, style.fail)
-				if(fatalError){
-					console.log(fatalError)
+	return function(description, suite){
+		console.log(`%c${description}:`, style.highlight)
+
+		const count = {
+			total: 0,
+			pass: 0,
+			fail: 0
+		}
+		const _assert = function(aInput, test, bInput){
+			count.total += 1
+
+			const aLog = (aInput instanceof Function ? stripFunctionText(aInput) : aInput)
+			const bLog = (bInput instanceof Function ? stripFunctionText(bInput) : bInput)
+			const testLog = stripFunctionText(test)
+				.replace(/\ba\b/g, aLog)
+				.replace(/\bb\b/g, bLog)
+
+			let aValue, bValue, didPass, fatalError
+			try{
+				aValue = (aInput instanceof Function ? aInput() : aInput)
+				bValue = (bInput instanceof Function ? bInput() : bInput)
+				if(test(aValue, bValue)){
+					didPass = true
 				}else{
-					if (aInput instanceof Function || bInput instanceof Function) {
-						console.log(aInput instanceof Function ? `\t${aLog}: ${aValue}` : `\t${aValue}`)
-						console.log(bInput instanceof Function ? `\t${bLog}: ${bValue}` : `\t${bValue}`)
+					throw new Error('fail')
+				}
+			}catch(e){
+				didPass = false
+				if(e.message !== 'fail'){
+					fatalError = e
+				}
+			}finally{
+				if(didPass){
+					count.pass += 1
+					console.log(`%c${count.total}.) ${testLog}`, style.pass)
+				}else{
+					count.fail += 1
+					console.log(`%c${count.total}.) ${testLog}`, style.fail)
+					if(fatalError){
+						console.log(fatalError)
+					}else{
+						if(aInput instanceof Function || bInput instanceof Function){
+							console.log(aInput instanceof Function ? `\t${aLog}: ${aValue}` : `\t${aValue}`)
+							console.log(bInput instanceof Function ? `\t${bLog}: ${bValue}` : `\t${bValue}`)
+						}
 					}
 				}
 			}
 		}
+
+		suite(_assert)
+		console.log(`%cFAILS: ${count.fail}`, style.fail)
+		console.log(`TOTAL: ${count.total}`)
 	}
-
-	suite(_assert)
-	console.log(`%cFAILS: ${count.fail}`, style.fail)
-	console.log(`TOTAL: ${count.total}`)
-}
-
-const stripFunctionText = function(inputFunction){
-	return inputFunction
-		.toString()
-		.replace(/^(async )?function.*?\{/, '')
-		.replace(/^.*?=>\s*\{?/, '')
-		.replace(/\}$/, '')
-		.trim()
-}
+})()
